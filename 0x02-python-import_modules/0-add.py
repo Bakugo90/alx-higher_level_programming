f5394da11149e7efd4461d3e6706ae7cefe2cfaa
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-10-16 23:28:38 +0000

    add.py correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/python3
+from add_0 import add
 
 if __name__ == "__main__":
-    # Imports the function add
-    from add_0 import add
+    """
 
-    # prints the result of the addition 1 + 2 = 3
+    Prints the result of the addition between two numbers
+
+    """
     a = 1
     b = 2
-
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
