671b9fe5eef3667e312fe3a0845d7b1137ff3103
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-10-16 23:41:35 +0000

    add.py correction

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from add_0 import add
+import add_0
 
 if __name__ == "__main__":
     """
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     """
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -9,4 +9,3 @@ def add(a, b):
         The return value. a + b
     """
     return (a + b)
-    
