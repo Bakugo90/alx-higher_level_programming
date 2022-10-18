9312a2eb12bf6dc45feb07b45b4f5506c677b215
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-10-18 20:55:47 +0000

    calcuation

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/bash
 from calculator_1 import add, sub, mul, div
 
-if __name__ == '__main__':
-    """
+if __name__ == "__main__":
 
-    Prints the result of the addition, substract, multiplication
-    and division between two numbers
+    """
+    imports functions from the file calculator_1.py, and does some Maths
 
     """
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
-    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
-    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
-    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
