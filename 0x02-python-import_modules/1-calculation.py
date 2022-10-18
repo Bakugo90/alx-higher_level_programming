8d96029a207d22ca2bb1a84f58740bb1768e8c03
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-10-18 22:12:35 +0000

    print and list all argument

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-if __name__ == "__main__":
-
+if __name__ == '__main__':
     """
-    imports functions from the file calculator_1.py, and does some Maths
+
+    Prints the result of the addition, substract, multiplication
+    and division between two numbers
 
     """
     a = 10
     b = 5
-
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
--- 0x02-python-import_modules/10-args.py
+++ 0x02-python-import_modules/10-args.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/puthon3
 import sys
 
 if __name__ == "__main__":
