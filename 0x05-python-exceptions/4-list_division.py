10e1c70cd80fbf6f99748e5968b67ec0013412d5
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-10-30 10:31:06 +0000

    4-lqit_division

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    """
+    A function that divides element by element 2 lists.
+    """
+    new_list = []
+
+    for i in range(list_length):
+        try:
+            new_list.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            new_list.append(0)
+            print('division by 0')
+            continue
+        except IndexError:
+            new_list.append(0)
+            print('out of range')
+            continue
+        except TypeError:
+            new_list.append(0)
+            print('wrong type')
+            continue
+        finally:
+            pass
+
+    return new_list
