40b5c5f4d60f3ed69230a4f7da3012f40334fbc3
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-10-30 09:25:59 +0000

    safe print intgers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from typing import Type
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """
+    A function that prints the first x elements of a list and only integers.
+    """
+    idx = 0
+
+    for i in range(x):
+        try:
+            if type(my_list[i]) is int and idx != x:
+                print("{:d}".format(my_list[i]), end="")
+                idx += 1
+
+        except TypeError:
+            continue
+    print()
+
+    return idx
