dfc49d62c45894fde79d1652f0b7d3930a4a806e
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-11-01 23:53:52 +0000

    doc strings

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Square Class
 
- A square class
+A Square Class
 
 """
 
@@ -9,18 +9,13 @@
 class Square:
 
     def __init__(self, size):
-
         """__init__
 
         The __init__ method initializes the size value
         of the square.
 
-        Args:
-            self: is used to point to object itself.
-            size: size of square
-
         Attributes:
-            __size (int): protected attributes.
+            size (int): The size of the square.
 
         """
         self.__size = size
