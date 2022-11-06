1b67e4eefddb6d817c0ab2ab905bc566a179d855
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-11-06 15:48:59 +0000

    protected typeError and raise error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,25 +1,9 @@
 #!/usr/bin/python3
-"""Square Class
-
- A square class
-
-"""
+"""tache 2 description d'une class vide"""
 
 
 class Square:
-
+    """ definition de la fonction init de square"""
     def __init__(self, size):
-
-        """__init__
-
-        The __init__ method initializes the size value
-        of the square.
-
-        Args:
-            size: size of square.
-
-        Attributes:
-            size (int): protected attrineutes instances.
-
-        """
+        """init function"""
         self.__size = size
