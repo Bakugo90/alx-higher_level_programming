4a7cbe2cfb28ac75cf2f88843b9c82ce6bef64ea
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-11-06 15:34:55 +0000

    protected typeError and raise error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
-"""Square
- A class that define a square
+"""Square Class
+
+A Square Class
 
 """
 
+
 class Square:
+
     def __init__(self, size=0):
         """__init__
 
-        The __init__ method initialize the size value of the square
+        The __init__ method initializes the size value of the square.
 
         Attributes:
             size (:obj:`int`, optional): The size of the square.
 
         Raises:
-            TypeError: if type of size is not `int`.
+            TypeError: If `size` type is not `int`.
 
-            ValueError: if the value of size is less than `0`.
+            ValueError: If `size` is less than `0`.
 
         """
 
         if type(size) is not int:
-            raise TypeError("size must be an integer")
-        if size <= 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+
+        if size < 0:
+            raise ValueError('size must be >= 0')
 
         self.__size = size
