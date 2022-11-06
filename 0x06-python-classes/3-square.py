d24db3f672e85e3ebbb7544b061944ec143fe137
SamadouOuroagorouko <Souroagorouko@gmail.com>
2022-11-06 16:45:27 +0000

    area of square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Sqaure
+A class that defines a sqaure.
+
+"""
+
+
+class Square:
+    """Defining the init method"""
+    def __init__(self, size=0):
+        """__init__
+
+        Initialize the size value to square.
+
+        Attributes:
+            size (int): the size of the square.
+
+        Raises:
+            TypeError: if the type of size is not 'int'.
+
+            ValueError: if the value ogf size is less than 0.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """This method return the are of square"""
+        return self.size**2
