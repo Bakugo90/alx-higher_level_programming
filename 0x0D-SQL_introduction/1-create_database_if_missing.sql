70ef890cc485bc0c486af7511f13f9711cceb19e
Samadou OURO-AGOROUKO <souroagorouko@gmail.com>
2022-11-30 14:48:43 -0800

    corrcton

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
---Create Database `hbtn_0c_0` on mysql server
-	--if the database is alrady exist, the script should not fail
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- Create Database `hbtn_0c_0` on mysql server
+-- if the database is alrady exist, the script should not fail
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
--- 0x0D-SQL_introduction/1-create_databases_if_missing.sql
+++
@@ -1,3 +0,0 @@
--- A script that create a database hbtn_0c_0 in mysql server
-	-- if not alrady exits
-CREATE DATABASE IF EXISTS hbtn_0c_0
