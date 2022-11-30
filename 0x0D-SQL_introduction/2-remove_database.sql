ec437b2bb8455274cdd0cd11d492c9ab5aad2e76
Samadou OURO-AGOROUKO <souroagorouko@gmail.com>
2022-11-30 14:51:49 -0800

    drop database correction

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
---the database `hbtn_0c_0` in MySQL Server.
+-- the database `hbtn_0c_0` in MySQL Server.
 --      If the database `hbtn_0c_0` doesn't exists, the script shouldn't fail
 DROP DATABASE IF EXISTS hbtn_0c_0;
