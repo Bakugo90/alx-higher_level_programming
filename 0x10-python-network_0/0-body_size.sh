0ca999f2910f707dd526e13d3019b341aacd70ea
SamadouOuroagorouko <Souroagorouko@gmail.com>
2023-01-16 23:49:46 +0000

    python network

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash env
 # a Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI "$1" | grep "Content-length" | cut -d' ' -f2
+curl -s "$1" | wc -c
